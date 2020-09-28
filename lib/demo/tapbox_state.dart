ce6f9c18faf4da8c3f075e7ea977aee5d9fbf01e
Wai Lee <liwei511@live.cn>
2020-09-28 18:36:41 +0800

    父子状态传递

--- lib/demo/tapbox_state.dart
+++ lib/demo/tapbox_state.dart
@@ -1,35 +1,63 @@
 import 'package:flutter/material.dart';
 import 'index.dart';
 
+// 混合状态管理
 class TapboxDemo extends StatefulWidget {
   TapboxState createState() => TapboxState();
 }
 
 class TapboxState extends State<TapboxDemo> {
   bool _active = false;
-  void _handleTap() {
+  void _handleTap(bool newValue) {
     setState(() {
-      _active = !_active;
+      _active = newValue;
     });
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text(DemoTitle['tapbox_state'])),
-        body: GestureDetector(
+        body: Column(
+          children: <Widget>[
+            TapboxChild(
+              active: _active,
+              handleTap: _handleTap,
+            ),
+            Text('父Widget管理子Widget的状态')
+          ]
+        )
+      );
+  }
+}
+
+class TapboxChild extends StatefulWidget {
+  TapboxChild({Key key, this.active: false, @required this.handleTap}) : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> handleTap;
+  TapboxChildState createState() => TapboxChildState();
+}
+class TapboxChildState extends State<TapboxChild> {
+
+  Widget build(BuildContext context) {
+
+    void _handleTap() {
+      widget.handleTap(!widget.active);
+    }
+    return GestureDetector(
           onTap: _handleTap,
           child: Container(
             child: Center(
-              child: Text(_active ? 'Active' : 'Inactive',
+              child: Text(widget.active ? 'Active' : 'Inactive',
                   style: TextStyle(
                       fontSize: 32,
-                      color: _active ? Colors.yellow : Colors.green)),
+                      color: widget.active ? Colors.yellow : Colors.green)),
             ),
             width: 200.0,
             height: 200.0,
             decoration: BoxDecoration(
-                color: _active ? Colors.lightGreen[700] : Colors.grey[600]),
+                color: widget.active ? Colors.lightGreen[700] : Colors.grey[600]),
           ),
-        ));
+        );
   }
 }
