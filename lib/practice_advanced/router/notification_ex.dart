bee6f45ef57850c5b010e09b17e525473157fbc7
Wai Lee <liwei511@live.cn>
2020-10-21 15:13:23 +0800

    自定义通知类

--- lib/practice_advanced/router/notification_ex.dart
+++ lib/practice_advanced/router/notification_ex.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 
-class NotificationExample extends StatelessWidget {
-  const NotificationExample({Key key});
+class NotificationExample extends StatefulWidget {
+  NotificationExample({Key key}) : super(key: key);
+
+  @override
+  _NotificationExampleState createState() => _NotificationExampleState();
+}
+
+class _NotificationExampleState extends State<NotificationExample> {
+  String _msg = '';
+  @override
   Widget build(BuildContext context) {
-    return Material(
-        child: ListView(
-      children: [ListTile(title: Text('手势识别'))],
-    ));
+    return NotificationListener<MyNotification>(
+      onNotification: (notification) {
+        setState(() {
+          _msg += notification.msg + '  ';
+        });
+        return true;
+      },
+      child: Center(
+        child: Column(
+          children: [
+            Builder(
+              builder: (context) {
+                return RaisedButton(
+                  child: Text('send notification'),
+                  onPressed: () => MyNotification('Hi').dispatch(context),
+                );
+              },
+            ),
+            Text(_msg)
+          ],
+        ),
+      ),
+    );
   }
+}
+
+// 自定义通知类
+class MyNotification extends Notification {
+  MyNotification(this.msg);
+  final String msg;
 }
